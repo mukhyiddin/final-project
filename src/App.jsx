bc3d2c999f98d63aa7ae05ccbfd6eea433a8e68f
mukhyiddin <yudivandiego@gmail.com>
2022-03-23 14:46:55 +0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+function App() {
+
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>
+          Content Are here...!!!
+        </h1>
+      </header>
+    </div>
+  );
+}
+
+
+
+export default App;
